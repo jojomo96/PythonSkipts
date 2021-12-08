3e763da42fc74214663c6ebf75fb25d1ad2302d7
moritjo <johannes.moritz@msg.group>
2021-12-08 11:20:44 +0100

    first bot test

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+import telebot
+import os
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+API_KEY = os.environ["API_KEY"]
+chatId = '1154921572'
+BOT = telebot.TeleBot(API_KEY)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def initBot():
+    BOT.send_message(chatId, "Test Nachricht")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    initBot()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
